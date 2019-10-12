5ad064bd6bd52a730ec63e4f8263a352c0a5c213
nikamuckova <nikolkamuckova@gmail.com>
2019-10-12 11:53:28 +0200

    Netlify form edit

--- src/components/contactForm2.js
+++ src/components/contactForm2.js
@@ -7,13 +7,13 @@ export default () => (
     <Form layout="vertical" name="contact" method="post" data-netlify="true">
       <input type="hidden" name="form-name" value="contact" />
       <Form.Item label="Name">
-        <Input size="large" name="name" />
+        <input name="name" />
       </Form.Item>
       <Form.Item label="Email">
-        <Input size="large" name="email" />
+        <input name="email" />
       </Form.Item>
       <Form.Item label="Message">
-        <Input.TextArea placeholder="Write the message" name="message" />
+        <textarea placeholder="Write the message" name="message" />
       </Form.Item>
       <Form.Item>
         <button type="submit" name="button" className={styles.formButton}>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,14 +5,14 @@ import Container from "../components/container"
 import ContactImage from "../images/undraw_contact_us.svg"
 import Footer from "../components/footer"
 import { Layout } from "../components/layout"
-import ContactForm from "../components/contactForm"
+import ContactForm2 from "../components/contactForm2"
 
 export default () => (
   <Layout>
     <Container>
       <Header headerText="Contact me!" />
       <div className={styles.flexContainer} style={{ flexWrap: "nowrap" }}>
-        <ContactForm />
+        <ContactForm2 />
         <img
           src={ContactImage}
           alt="Programming girl"
