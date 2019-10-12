d43d05a3ae0a8b54ed872e1bd76c51dc0ebeaa9b
nikamuckova <nikolkamuckova@gmail.com>
2019-10-12 12:01:32 +0200

    Netlifu form edit

--- src/components/contactForm2.js
+++ src/components/contactForm2.js
@@ -7,13 +7,13 @@ export default () => (
     <form layout="vertical" name="contact" method="post" data-netlify="true">
       <input type="hidden" name="form-name" value="contact" />
       <Form.Item label="Name">
-        <input name="name" />
+        <Input size="large" name="name" />
       </Form.Item>
       <Form.Item label="Email">
-        <input name="email" />
+        <Input size="large" name="email" />
       </Form.Item>
       <Form.Item label="Message">
-        <textarea placeholder="Write the message" name="message" />
+        <Input.TextArea placeholder="Write the message" name="message" />
       </Form.Item>
       <Form.Item>
         <button type="submit" name="button" className={styles.formButton}>
