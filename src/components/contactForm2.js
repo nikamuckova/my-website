4b639546a105ca5ced70a48597eb38f591f10543
nikamuckova <nikolkamuckova@gmail.com>
2019-10-12 09:46:39 +0200

    Netlify form  - adding name attribute

--- src/components/contactForm2.js
+++ src/components/contactForm2.js
@@ -5,13 +5,13 @@ import styles from "./mystyle.module.css"
 export default () => (
   <div className={styles.formBorder} style={{ width: "100%" }}>
     <Form layout="vertical" name="contact" method="POST" data-netlify="true">
-      <Form.Item label="Name">
+      <Form.Item label="Name" name="name">
         <Input size="large" />
       </Form.Item>
-      <Form.Item label="Email">
+      <Form.Item label="Email" name="email">
         <Input size="large" />
       </Form.Item>
-      <Form.Item label="Message">
+      <Form.Item label="Message" name="message">
         <Input.TextArea placeholder="Write the message" />
       </Form.Item>
       <Form.Item>
