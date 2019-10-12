11620e5876f95e0a07f065baef9333677e8dbd1f
nikamuckova <nikolkamuckova@gmail.com>
2019-10-12 12:20:16 +0200

    Netlify form edit

--- src/components/contactForm2.js
+++ src/components/contactForm2.js
@@ -4,24 +4,22 @@ import styles from "./mystyle.module.css"
 
 export default () => (
   <div className={styles.formBorder} style={{ width: "100%" }}>
-    <form name="contact" method="post" data-netlify="true">
+    <form layout="vertical" name="contact" method="post" data-netlify="true">
       <input type="hidden" name="form-name" value="contact" />
-      <Form layout="vertical">
-        <Form.Item label="Name">
-          <Input size="large" name="name" />
-        </Form.Item>
-        <Form.Item label="Email">
-          <Input size="large" name="email" />
-        </Form.Item>
-        <Form.Item label="Message">
-          <Input.TextArea placeholder="Write the message" name="message" />
-        </Form.Item>
-        <Form.Item>
-          <button type="submit" name="button" className={styles.formButton}>
-            SEND MESSAGE
-          </button>
-        </Form.Item>
-      </Form>
+      <Form.Item label="Name">
+        <Input size="large" name="name" />
+      </Form.Item>
+      <Form.Item label="Email">
+        <Input size="large" name="email" />
+      </Form.Item>
+      <Form.Item label="Message">
+        <Input.TextArea placeholder="Write the message" name="message" />
+      </Form.Item>
+      <Form.Item>
+        <button type="submit" name="button" className={styles.formButton}>
+          SEND MESSAGE
+        </button>
+      </Form.Item>
     </form>
   </div>
 )
