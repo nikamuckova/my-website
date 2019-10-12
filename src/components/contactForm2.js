819be038a99264edba1196182fc2836e7cf8ed9c
nikamuckova <nikolkamuckova@gmail.com>
2019-10-12 10:06:18 +0200

    Netlify form - name attribute correction

--- src/components/contactForm2.js
+++ src/components/contactForm2.js
@@ -5,14 +5,14 @@ import styles from "./mystyle.module.css"
 export default () => (
   <div className={styles.formBorder} style={{ width: "100%" }}>
     <Form layout="vertical" name="contact" method="POST" data-netlify="true">
-      <Form.Item label="Name" name="name">
-        <Input size="large" />
+      <Form.Item label="Name">
+        <Input size="large" name="name" />
       </Form.Item>
-      <Form.Item label="Email" name="email">
-        <Input size="large" />
+      <Form.Item label="Email">
+        <Input size="large" name="email" />
       </Form.Item>
-      <Form.Item label="Message" name="message">
-        <Input.TextArea placeholder="Write the message" />
+      <Form.Item label="Message">
+        <Input.TextArea placeholder="Write the message" name="message" />
       </Form.Item>
       <Form.Item>
         <button type="submit" className={styles.formButton}>
