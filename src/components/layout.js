ec3814c1b6d10ebbadfda0ca131461ef58648812
nikamuckova <nikolkamuckova@gmail.com>
2019-10-18 00:48:00 +0200

    Add helmet and title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Navigation from "./navigation"
 import NavigationSmall from "./navigationSmall"
 import styles from "../components/mystyle.module.css"
@@ -8,18 +9,24 @@ import "./layout.css"
 import "antd/dist/antd.css"
 
 export const Layout = ({ children }) => (
-  <div className={styles.body}>
-    <div className={styles.wrapper}>
-      <div className={styles.largeMenu}>
-        <Navigation />
+  <>
+    <Helmet
+      title="Nika Muckova"
+      description="I am a 25 years old Junior Front-end developer."
+    />
+    <div className={styles.body}>
+      <div className={styles.wrapper}>
+        <div className={styles.largeMenu}>
+          <Navigation />
+        </div>
+        <div className={styles.smallMenu}>
+          <NavigationSmall />
+        </div>
+        <div className={styles.main}>{children}</div>
       </div>
-      <div className={styles.smallMenu}>
-        <NavigationSmall />
-      </div>
-      <div className={styles.main}>{children}</div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
+  </>
 )
 
 /*TO DO:
