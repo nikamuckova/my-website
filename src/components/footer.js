cabfe9b74efd8ca5255b18a80084d876daca5dd6
nikamuckova <nikolkamuckova@gmail.com>
2019-10-18 00:48:22 +0200

    Fix icons target

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,17 +4,37 @@ import { Icon } from "antd"
 
 export default () => (
   <div className={styles.footer}>
-    <a href="https://github.com/nikamuckova">
-      <Icon type="github" className={styles.media} />
+    <a
+      href="https://github.com/nikamuckova"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.media}
+    >
+      <Icon type="github" />
     </a>
-    <a href="https://www.linkedin.com/in/nikola-m%C3%BA%C4%8Dkov%C3%A1-16991812a/">
-      <Icon type="linkedin" className={styles.media} />
+    <a
+      href="https://www.linkedin.com/in/nikola-m%C3%BA%C4%8Dkov%C3%A1-16991812a/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.media}
+    >
+      <Icon type="linkedin" />
     </a>
-    <a href="https://www.instagram.com/nikamuckova/">
-      <Icon type="instagram" className={styles.media} />
+    <a
+      href="https://www.instagram.com/nikamuckova/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.media}
+    >
+      <Icon type="instagram" />
     </a>
-    <a href="https://twitter.com/NikolaMuckova">
-      <Icon type="twitter" className={styles.media} />
+    <a
+      href="https://twitter.com/NikolaMuckova"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.media}
+    >
+      <Icon type="twitter" />
     </a>
   </div>
 )
