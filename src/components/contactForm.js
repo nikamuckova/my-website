12ad8751daa089b3090ea8efab79bb5dea0383db
nikamuckova <nikolkamuckova@gmail.com>
2019-10-18 10:27:43 +0200

    Form - required inputs

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -7,13 +7,17 @@ export default () => (
     <form name="contact" method="post" data-netlify="true" action="/thanks">
       <input type="hidden" name="form-name" value="contact" />
       <Form.Item label="Name">
-        <Input size="large" name="name" />
+        <Input size="large" name="name" required />
       </Form.Item>
       <Form.Item label="Email">
-        <Input size="large" name="email" />
+        <Input size="large" name="email" type="email" required />
       </Form.Item>
       <Form.Item label="Message">
-        <Input.TextArea placeholder="Write the message" name="message" />
+        <Input.TextArea
+          placeholder="Write the message"
+          name="message"
+          required
+        />
       </Form.Item>
       <Form.Item>
         <button type="submit" name="button" className={styles.formButton}>
