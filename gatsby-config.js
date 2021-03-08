8ec922a5a22a9ee6822c2d4fc65047d878cf34b1
Nikola Múčková <nikola.muckova@01people.com>
2021-03-08 22:42:57 +0100

    font fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,25 +6,4 @@
 
 module.exports = {
   plugins: [`gatsby-plugin-netlify`, `gatsby-plugin-react-helmet`],
-  plugins: [
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Indie Flower`,
-            variants: [`400`, `700`]
-          },
-          {
-            family: `Noto Sans HK`,
-            variants: [`400`, `700`]
-          },
-          {
-            family: `Roboto`,
-            subsets: [`latin`]
-          },
-        ],
-      },
-    }
-  ]
 }
